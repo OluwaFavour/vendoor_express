6d8e6fa2b7106806df696ded4f2c1ba9385f2a60
Favour Olusayo <fsticks8187@gmail.com>
2024-07-30 23:37:16 +0100

    feat: Upload and set logo, proof of identity, and business registration certificate images for new shop during creation

--- app/crud/shop.py
+++ app/crud/shop.py
@@ -27,36 +27,37 @@ def create_shop(db: Session, form_data: VendorProfileCreationForm, user: User) -
     proof_of_identity_type: ProofOfIdentityType = form_data.pop(
         "proof_of_identity_type"
     )
-
-    # Upload Image to cloudinary
     proof_of_identity_image: UploadFile = form_data.pop("proof_of_identity_image")
-    proof_of_identity_image_url = upload_image(
-        str(user.id), proof_of_identity_image.file
-    )
-
-    # Upload Image to cloudinary
     business_registration_certificate_image: UploadFile = form_data.pop(
         "business_registration_certificate_image"
     )
-    business_registration_certificate_image_url = upload_image(
-        str(user.id), business_registration_certificate_image.file
-    )
+    logo: UploadFile = form_data.pop("logo")
+    shop = Shop(**form_data, vendor_id=user.id)
+
     user = update_user(
         db,
         user,
         phone_number=user_phone_number,
         role=user_role,
         proof_of_identity_type=proof_of_identity_type,
-        proof_of_identity_image=proof_of_identity_image_url,
-        business_registration_certificate_image=business_registration_certificate_image_url,
         is_shop_owner=True,
     )
-    logo: UploadFile = form_data.pop("logo")
 
-    shop = Shop(**form_data, vendor_id=user.id)
-    # Upload Image to cloudinary
-    logo: str = upload_image(str(shop.id), logo.file)
-    shop.logo = logo
+    # Upload images
+    proof_of_identity_image_url = upload_image(
+        f"{user.id}/verification/{proof_of_identity_type.name}",
+        proof_of_identity_image.file,
+    )
+    business_registration_certificate_image_url = upload_image(
+        f"{user.id}/verification/business_registration_certificate",
+        business_registration_certificate_image.file,
+    )
+    logo_url: str = upload_image(f"{user.id}/shop/logo", logo.file)
+    shop.logo = logo_url
+    user.proof_of_identity_image = proof_of_identity_image_url
+    user.business_registration_certificate_image = (
+        business_registration_certificate_image_url
+    )
     db.add(shop)
     db.commit()
     db.refresh(shop)
@@ -76,7 +77,9 @@ def update_shop(db: Session, shop: Shop, **kwargs) -> Shop:
     if "logo" in values:
         values["logo"] = upload_image(str(shop.id), values["logo"].file)
     if "cover_photo" in values:
-        values["cover_photo"] = upload_image(str(shop.id), values["cover_photo"].file)
+        values["cover_photo"] = upload_image(
+            f"{shop.vendor_id}/shop/cover_photo", values["cover_photo"].file
+        )
     db.execute(update(Shop).filter_by(id=shop.id).values(**values))
     db.commit()
     return shop
@@ -105,7 +108,7 @@ def add_staff_to_shop(db: Session, shop: Shop, **kwargs) -> ShopMember:
     staff = ShopMember(**kwargs)
     if "profile_image" in kwargs:
         kwargs["profile_image"] = upload_image(
-            str(staff.id), kwargs["profile_image"].file
+            f"{shop.vendor_id}/shop/staff/{staff.id}", kwargs["profile_image"].file
         )
     shop.members.append(staff)
     db.commit()
@@ -121,7 +124,8 @@ def update_shop_staff(db: Session, staff: ShopMember, **kwargs) -> ShopMember:
         values[key] = value
     if "profile_image" in values:
         values["profile_image"] = upload_image(
-            str(staff.id), values["profile_image"].file
+            f"{staff.shop.vendor_id}/shop/staff/{staff.id}",
+            values["profile_image"].file,
         )
     db.execute(update(ShopMember).filter_by(id=staff.id).values(**values))
     db.commit()
