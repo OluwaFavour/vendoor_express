0f3dd7a39135f383e68752de66c1dcfba2ee6e63
Favour Olusayo <fsticks8187@gmail.com>
2024-08-19 15:48:30 +0100

    feat: Add login functionality for admin users
    This commit adds the `/login` endpoint to the `admin.py` module in the `api` directory. The endpoint handles the login functionality for admin users. It verifies the user's role and creates a new session for the user. The session ID is stored in a cookie and returned in the response. This addition enhances the security and authentication process for admin users.

--- app/api/admin.py
+++ app/api/admin.py
@@ -1,16 +1,23 @@
-from datetime import datetime
+from datetime import datetime, timedelta, UTC
 import uuid
 from typing import Annotated, Optional
 
 from fastapi import APIRouter, Depends, HTTPException, status, Query, Body
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 from sqlalchemy import and_, or_, func
 from sqlalchemy.orm import Session
 from sqlalchemy.future import select
 
 from ..dependencies import get_db, get_current_active_admin
-from ..crud.user import get_user
+from ..crud.user import get_user, update_user
 from ..crud.shop import get_shop
+from ..core.config import settings
 from ..core.debug import logger
+from ..core.utils import delete_session_by_user_id, create_session, authenticate
+from ..crud.session import (
+    delete_session,
+)
 from ..db.enums import (
     FilterOperatorType,
     UserRoleType,
@@ -24,6 +31,75 @@ from ..schemas.user import User, UserPage, SortField
 from ..schemas.shop import Shop, ShopPage
 
 router = APIRouter(prefix="/api/admin", tags=["admin"])
+
+
+@router.post("/login", status_code=status.HTTP_200_OK)
+def admin_login(
+    user: Annotated[UserModel, Depends(authenticate)],
+    db: Annotated[Session, Depends(get_db)],
+    request: Request,
+):
+    if user.role != UserRoleType.ADMIN.value:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is not an admin",
+        )
+    # Get session data
+    data = str(user.id)
+
+    # Delete old session IDs if they exists
+    delete_session_by_user_id(db, data)
+    if "session_id" in request.cookies:
+        old_session_id = request.cookies["session_id"]
+        delete_session(db, uuid.UUID(old_session_id))
+
+    # Get user agent and IP address
+    user_agent = request.headers.get("User-Agent")
+    ip_address = request.client.host
+
+    # Generate session ID
+    session_id = str(uuid.uuid4())
+    # Store session
+    expires_at = settings.session_expire_days
+    expiry = datetime.now(UTC) + timedelta(days=expires_at)
+    session_id = create_session(
+        db=db,
+        id=session_id,
+        data=data,
+        expires_at=expiry,
+        user_agent=user_agent,
+        ip_address=ip_address,
+    )
+    request.session["session_id"] = session_id
+
+    # Update user's first login status
+    if user.is_first_login is None:
+        update_user(db, user, is_first_login=True)
+    else:
+        update_user(db, user, is_first_login=False)
+
+    # Create response
+    response = JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "message": "Successfully logged in",
+            "user_agent": user_agent,
+            "ip_address": ip_address,
+            "is_first_login": user.is_first_login,
+        },
+    )
+    # Store new session ID in cookie
+    response.set_cookie(
+        key=settings.session_cookie_name,
+        value=session_id,
+        httponly=True,
+        samesite=settings.same_site,
+        max_age=(expires_at * 24 * 60 * 60),
+        secure=settings.https_only,
+    )
+
+    # Return response
+    return response
 
 
 @router.get(
